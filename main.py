b7d290c53760627cfaef7faa60de9855581313b7
gersondantas <gerson.accari@hotmail.com>
2024-08-05 17:20:34 -0300

    refactor: remove unnecessary coments

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from gensim.models import Word2Vec
 from gensim.similarities import WmdSimilarity
 import os
 
-# Configuring the logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -15,10 +14,8 @@ app = FastAPI()
 class QueryModel(BaseModel):
     query: str
 
-# CSV Path
 csv_path = os.path.join(os.path.dirname(__file__), 'qa_base.csv')
 
-# Load the CSV using pandas
 try:
     df = pd.read_csv(csv_path)
     logger.info("CSV carregado com sucesso.")
@@ -26,7 +23,6 @@ except Exception as e:
     logger.error(f"Erro ao carregar o CSV: {e}")
     raise
 
-# Pre-processing of questions and answers
 try:
     questions = df['Pergunta'].apply(lambda x: x.split()).tolist()
     answers = df['Resposta'].tolist()
@@ -39,7 +35,6 @@ except Exception as e:
     logger.error(f"Erro no pré-processamento: {e}")
     raise
 
-# Training the Word2vec model
 try:
     model = Word2Vec(sentences=questions, vector_size=100, window=5, min_count=1, workers=4)
     logger.info("Modelo Word2Vec treinado com sucesso.")
@@ -47,7 +42,6 @@ except Exception as e:
     logger.error(f"Erro ao treinar o modelo Word2Vec: {e}")
     raise
 
-# Creating the similarity index
 try:
     index = WmdSimilarity(questions, model.wv, num_best=3)
     logger.info("Índice de similaridade criado com sucesso.")
@@ -58,7 +52,7 @@ except Exception as e:
 def retrieve_info(query):
     query_tokens = query.split()
     try:
-        similar_response = index[query_tokens]  # Taking the 3 most similar documents
+        similar_response = index[query_tokens] 
         return [qa_pairs[int(doc_id)][1] for doc_id, sim in similar_response]
     except Exception as e:
         logger.error(f"Erro ao recuperar informações: {e}")
@@ -67,7 +61,7 @@ def retrieve_info(query):
 def generate_response(query):
     try:
         similar_responses = retrieve_info(query)
-        response = " ".join(similar_responses)  # Here we can improve the response generated
+        response = " ".join(similar_responses)
         return response
     except Exception as e:
         logger.error(f"Erro ao gerar resposta: {e}")
